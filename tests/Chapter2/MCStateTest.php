4e1c5b53bdb628daf8fe573b98b948bf37a1b134
Sascha Kersken <kersken@dimensional.de>
2020-06-06 09:57:55 +0200

    More unit tests

--- tests/Chapter2/MCStateTest.php
+++ tests/Chapter2/MCStateTest.php
@@ -23,4 +23,48 @@ The boat is on the west bank.";
     $mc = new MCState(3, 3, TRUE);
     $this->assertSame($expected, $mc->__toString());
   }
+
+  /**
+  * @covers MCState::isLegal
+  * @dataProvider isLegalProvider
+  */
+  public function testIsLegal($wm, $wc, $expected) {
+    $mc = new MCState($wm, $wc, TRUE);
+    $this->assertEquals($expected, $mc->isLegal());
+  }
+
+  public function isLegalProvider() {
+    return [
+      [1, 2, FALSE],
+      [2, 1, FALSE],
+      [3, 3, TRUE]
+    ];
+  }
+
+  /**
+  * @covers MCState::__get
+  * @dataProvider getProvider
+  */
+  public function testGet($property, $expected) {
+    $mc = new MCState(3, 3, TRUE);
+    $this->assertEquals($expected, $mc->$property);
+  }
+
+  public function getProvider() {
+    return [
+      ['wm', 3],
+      ['wc', 3],
+      ['em', 0],
+      ['ec', 0],
+      ['boat', TRUE]
+    ];
+  }
+
+  /**
+  * @covers MCState::goalTest
+  */
+  public function testGoalTest() {
+    $mc = new MCState(0, 0, FALSE);
+    $this->assertTrue(MCState::goalTest($mc));
+  }
 }
