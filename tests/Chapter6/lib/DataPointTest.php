e40b19b9db4c8bdeab41705b62ac1aa34a3c0f2e
Sascha Kersken <kersken@dimensional.de>
2020-06-10 08:09:32 +0200

    Started ch. 6 unit tests

---
+++ tests/Chapter6/lib/DataPointTest.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once(__DIR__.'/../../../Util.php');
+
+use PHPUnit\Framework\TestCase;
+
+final class DataPointTest extends TestCase {
+  /**
+  * @covers DataPoint::__construct
+  */
+  public function testConstruct() {
+    $dp = new DataPoint([10, 100]);
+    $this->assertInstanceOf('DataPoint', $dp);
+  }
+
+  /**
+  * @covers DataPoint::__get
+  */
+  public function testGet() {
+    $dp = new DataPoint([10, 100]);
+    $this->assertEquals(2, $dp->numDimensions);
+  }
+
+  /**
+  * @covers DataPoint::distance
+  */
+  public function testDistance() {
+    $dp1 = new DataPoint([2, 2]);
+    $dp2 = new DataPoint([5, 2]);
+    $this->assertEquals(3, $dp1->distance($dp2));
+  }
+
+  /**
+  * @covers DataPoint::__toString
+  */
+  public function testToString() {
+    $dp = new DataPoint([23, 42]);
+    $this->assertEquals('23, 42', $dp->__toString());
+  }
+}
